d7e658ad3d0e60d686d3c96a048fd44772c79ef9
fournest sebastien <sebfour85@gmail.com>
2025-05-20 12:31:59 +0200

    jusqu'à exercice 12

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log(`le produit est de ${produit}.`);
 
 // exercice 3
 let nombre = prompt(`Entrez un nombre`)
-if (nombre.toLowerCase()>10) {
+if (nombre.toLowerCase() > 10) {
     console.log(`Plus grand que 10`);
 
 } else {
@@ -24,7 +24,7 @@ if (nombre.toLowerCase()>10) {
 };
 
 // exercice 4
-// let nombre = 3485;
+let nombres = 3485;
 if (nombre % 2 === 0) {
     console.log(`Ce nombre est pair`);
 
@@ -44,3 +44,80 @@ if (maturite < 18) {
 } else {
     console.log(`Tu es senior`);
 }
+// exercice 6
+for (let i = 1; i <= 10; i++) {
+    console.log(`ligne ` + i);
+}
+// exercice 7
+
+let chiffre = prompt(`Entrez un chiffre`);
+for (let i = 0; i < 10; i++) {
+    let resultat = chiffre * i;
+    console.log(`${chiffre} * ${i} = ${resultat}`);
+
+}
+// exercice 8
+
+let nbre = 0;
+let sum = 0;
+for (let nbre = 0; nbre <= 100; nbre++) {
+    sum = sum + nbre;
+}
+console.log(`le somme est de ${sum}`);
+
+// exercice 9
+let nbr = prompt(`Devinez un nombre entre 1 et 10 ?`);
+let result = Math.floor(Math.random() * 10) + 1;
+nbr = parseInt(nbr)
+while (nbr !== 3) {
+    if (nbr > result) {
+        nbr = prompt(`Essaye encore`);
+    } else {
+        nbr = prompt(`Essaye encore`);
+    }
+
+    nbr = parseInt(nbr);
+}
+console.log(`Bravo, c'est correct`);
+
+
+
+
+// exercice 10
+
+for (let i = 0; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+// exercice 11
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+
+
+}
+console.log(` C'est terminé !`);
+
+// exercice 12
+let a = prompt(`Donnez moi un nombre svp.`);
+let b = prompt(`Donnez moi un autre nombre`);
+let operation = prompt(`Quelle operation voulez vous faire? (+, -, *, /)`);
+a = parseInt(a)
+b = parseInt(b)
+let resultat;
+switch (operation) {
+    case `+`:
+        resultat = a + b;
+        break;
+    case `-`:
+        resultat = a - b;
+        break;
+    case `*`:
+        resultat = a * b;
+        break;
+    case `/`:
+        resultat = a / b;
+        break;
+}
+console.log(`le resultat de ${a} ${operation} ${b} est ${resultat}`)
